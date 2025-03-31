aec5cc9940a787a94d6a2729820fe46289c00755
Artsem Barushka <Artsem.Barushka@coxautoinc.com>
2025-04-01 01:50:05 +0400

    update Main component to use MovieTile and MoviesCounter components;

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,23 @@ import {listOfGenres} from "../data/movieGenres";
 import GenreSelect from "./GenreSelect";
 import '../styles/main.css';
 import {useState} from "react";
+import MovieTile from "./MovieTile";
+import {listOfMoviesMock} from "../data/moviesListMock";
+import MoviesCounter from "./MoviesCounter";
 
 export default function Main() {
 
     const [selectedGenre, setSelectedGenre] = useState('horror');
+    const [movies, setMovies] = useState(listOfMoviesMock);
 
     const onSelect = (selectedGenre) => {
         console.log('the following genre is clicked: ', selectedGenre);
         setSelectedGenre(selectedGenre);
     };
+
+    const onMovieClick = (clickedMovie) => {
+        console.log('movie is clicked: ', clickedMovie);
+    }
 
     return (
         <div className={'main-content-wrapper'}>
@@ -20,6 +28,14 @@ export default function Main() {
                 selectHandler={onSelect}
             >
             </GenreSelect>
+
+            <MoviesCounter movies={movies}>
+            </MoviesCounter>
+
+            <MovieTile
+                movies={movies}
+                clickHandler={onMovieClick}>
+            </MovieTile>
         </div>
     );
 }
