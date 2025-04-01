0917c78776bafeb813951c3ebf849665a31740f1
Artsem Barushka <Artsem.Barushka@coxautoinc.com>
2025-04-01 17:11:44 +0400

    wire MovieDetailsHeader and MovieDetails components;

--- src/App.js
+++ src/App.js
@@ -3,16 +3,41 @@ import {Counter} from "./components/Counter";
 import Header from "./components/Header";
 import './styles/header.css';
 import Main from "./components/Main";
+import {useState} from "react";
+import MovieDetails from "./components/movie-details/MovieDetails";
+import MovieDetailsHeader from "./components/movie-details/header/MovieDetailsHeader";
 
 function App() {
-  return (
-      <div className={'app'}>
-          <Counter initialCounter={123}></Counter>
-          <Header></Header>
-          <div className={'header-from-main-divider'}></div>
-          <Main></Main>
-      </div>
-  );
+
+    const [movieSelected, setMovieSelected] = useState(undefined);
+
+    const onMovieSelect = (selectedMovie) => {
+        setMovieSelected(selectedMovie);
+    }
+
+    const handleSearchAll = () => {
+        setMovieSelected(undefined);
+    }
+
+    return (
+        <div className={'app'}>
+            <Counter initialCounter={123}></Counter>
+
+            {!movieSelected && <Header></Header>}
+            {movieSelected && (
+                <>
+                    <MovieDetailsHeader
+                        searchAllHandler={handleSearchAll}>
+                    </MovieDetailsHeader>
+                    <MovieDetails
+                        movie={movieSelected}>
+                    </MovieDetails>
+                </>
+            )}
+            <div className={'header-from-main-divider'}></div>
+            <Main onMovieSelect={onMovieSelect}></Main>
+        </div>
+    );
 }
 
 export default App;
