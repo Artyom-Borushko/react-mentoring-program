cd9907ac43ed8411de5b84a6a606280864f41863
Artsem Barushka <Artsem.Barushka@coxautoinc.com>
2025-04-01 17:11:22 +0400

    add MovieDetailsHeader component;

---
+++ src/stories/MovieDetailsHeader.stories.jsx
@@ -0,0 +1,12 @@
+import { fn } from '@storybook/test';
+import MovieDetailsHeader from "../components/movie-details/header/MovieDetailsHeader";
+
+export default {
+    component: MovieDetailsHeader,
+};
+
+export const Default = {
+    args: {
+        searchAllHandler: fn()
+    }
+};
