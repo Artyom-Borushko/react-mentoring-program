3b2cabdfabd1bd2a031519a69e0e104c8ae0c9ef
Artsem Barushka <Artsem.Barushka@coxautoinc.com>
2025-04-02 22:21:32 +0400

    add SortControl component;

---
+++ src/stories/SortControl.stories.jsx
@@ -0,0 +1,13 @@
+import { fn } from '@storybook/test';
+import SortControl from "../components/movie-tile-sort/sorters/options-sorter/SortControl";
+
+export default {
+    component: SortControl,
+};
+
+export const Default = {
+    args: {
+        selectedSortOption: 'test option selection',
+        sortControlHandler: fn()
+    }
+};
