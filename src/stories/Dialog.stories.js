2d9fcf34de669badb8885be52ceecf847671a023
Artsem Barushka <Artsem.Barushka@coxautoinc.com>
2025-04-10 02:27:48 +0400

    add Dialog component;

---
+++ src/stories/Dialog.stories.js
@@ -0,0 +1,57 @@
+import { fn } from '@storybook/test';
+import Dialog from "../components/shared/dialog/Dialog";
+import MovieForm from "../components/movie-form/MovieForm";
+import {listOfMoviesMock} from "../data/moviesListMock";
+import Button from "../components/shared/button/Button";
+import React from "react";
+
+export default {
+    component: Dialog,
+};
+
+export const Default = {
+    args: {
+        title: 'testTitle123',
+        onClose: fn()
+    }
+};
+
+export const AddEmptyMovie = {
+    args: {
+        title: 'Add Movie',
+        children: <MovieForm/>,
+        onClose: fn()
+    }
+};
+
+export const EditPredefinedMovie = {
+    args: {
+        title: 'Edit Movie',
+        children: <MovieForm initialMovie={listOfMoviesMock[4]} onSubmit={fn()}/>,
+        onClose: fn()
+    }
+};
+
+export const DeleteMovie = {
+    args: {
+        title: 'Delete Movie',
+        children: (
+            <>
+                <p
+                    className={'delete-movie-confirmation-text'}
+                >
+                    Are you sure you want to delete this movie?
+                </p>
+                <div className={'movie-form-confirmation-control'}>
+                    <Button
+                        type={'submit'}
+                        onClick={fn()}
+                    >
+                        Confirm
+                    </Button>
+                </div>
+            </>
+        ),
+        onClose: fn()
+    }
+};
