79961648ae2a7cb4e293be497a84a947ed6b7904
Artsem Barushka <Artsem.Barushka@coxautoinc.com>
2025-04-22 22:55:04 +0400

    install react router;

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <p>Route Not Found</p>
+    },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
