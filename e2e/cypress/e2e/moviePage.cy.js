782fd94cecf0f009afb3e1dd6812ec8f99680c3e
Artsem Barushka <Artsem.Barushka@coxautoinc.com>
2025-04-25 23:52:55 +0400

    add e2e tests for Movie Details with url pathname;

--- e2e/cypress/e2e/moviePage.cy.js
+++ e2e/cypress/e2e/moviePage.cy.js
@@ -14,7 +14,7 @@ describe('Movie page tests', () => {
         cy.url().should('equal', 'http://localhost:3000/?filter=comedy');
     });
 
-    it('selecting genre change the url and highlighting of selected genre on UI', () => {
+    it('selecting "all" genre remove filter from the url and highlighting of selected genre on UI', () => {
         cy.get('.genre-button[name="comedy"]').should('not.have.class', 'selected');
 
         cy.get('.genre-button[name="comedy"]').click();
@@ -44,14 +44,12 @@ describe('Movie page tests', () => {
     });
 
     it('selecting sort option change the url and displayed on UI name', () => {
-        let sortByDisplayedText = cy.get('.sort-control-wrapper').find('p');
-        sortByDisplayedText.should('have.text', 'Select sorting');
+        cy.get('.sort-control-wrapper').find('p').should('have.text', 'Select sorting');
 
         cy.get('.movie-expand-options-icon.movies-sort').click();
         cy.get('.dropdown-option').contains('release date').click();
 
-        sortByDisplayedText = cy.get('.sort-control-wrapper').find('p');
-        sortByDisplayedText.should('have.text', 'release date');
+        cy.get('.sort-control-wrapper').find('p').should('have.text', 'release date');
 
         cy.url().should('equal', 'http://localhost:3000/?sortBy=release_date&sortOrder=desc');
     });
@@ -81,4 +79,32 @@ describe('Movie page tests', () => {
         cy.get('input.search-input').should('have.value', 'te');
         cy.get('.sort-control-wrapper').find('p').should('have.text', 'release date');
     });
-})
+
+    describe('Movie Details tests', () => {
+        it('default route shows search input on UI', () => {
+            cy.get('input.search-input').should('have.attr', 'placeholder', 'What do you want to watch?');
+        });
+
+        it('clicking on movie, then url is updated with movieId and Movie Details header is present on UI', () => {
+            cy.get('.movie-card').first().click();
+            cy.url().should('equal', 'http://localhost:3000/337167');
+            cy.get('.movie-details-description').should('have.text', 'Believing they have left behind shadowy figures from their past, newlyweds Christian and Ana fully embrace an inextricable connection and shared life of luxury. But just as she steps into her role as Mrs. Grey and he relaxes into an unfamiliar stability, new threats could jeopardize their happy ending before it even begins.');
+        });
+
+        it('clicking on movie with active filters applied, then url is updated with movieId and filters query string remains untouched', () => {
+            cy.get('.genre-button[name="comedy"]').click();
+            cy.get('.movie-card').first().click();
+            cy.url().should('equal', 'http://localhost:3000/269149?filter=comedy');
+            cy.get('.movie-details-description').should('have.text', 'Determined to prove herself, Officer Judy Hopps, the first bunny on Zootopia\'s police force, jumps at the chance to crack her first case - even if it means partnering with scam-artist fox Nick Wilde to solve the mystery.');
+        });
+
+        it('clicking on search all movies, then movieId is removed from the url and filters query string remains untouched', () => {
+            cy.get('.genre-button[name="comedy"]').click();
+            cy.get('.movie-card').first().click();
+            cy.get('.movie-details-search-icon').click();
+            cy.url().should('equal', 'http://localhost:3000/?filter=comedy');
+            cy.get('input.search-input').should('have.attr', 'placeholder', 'What do you want to watch?');
+        });
+    });
+
+});
